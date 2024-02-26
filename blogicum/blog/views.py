9db1f7a0eb38120fedd959ce622318fb16a9f130
Evgeny Cherednichenko <More1ggg@yandex.ru>
2024-02-26 09:04:55 +0300

    Update blogicum

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -5,7 +5,7 @@ from blog.models import Category, Post
 from . import const
 
 
-def filt_func(postmanager):
+def filter_pub_post(postmanager):
     return postmanager.select_related(
         'author',
         'location',
@@ -18,25 +18,25 @@ def filt_func(postmanager):
 
 
 def index(request):
-    post_list = filt_func(Post.objects)[:const.SLICE]
+    post_list = filter_pub_post(Post.objects)[:const.POSTS_ON_PAGE]
     return render(request, 'blog/index.html', {'post_list': post_list})
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(
-        filt_func(Post.objects),
+        filter_pub_post(Post.objects),
         pk=post_id,
     )
     return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
-    post_list = filt_func(Category.objects.get(slug=category_slug).posts)
     category = get_object_or_404(
         Category,
         slug=category_slug,
         is_published=True
     )
+    post_list = filter_pub_post(category.posts)
     return render(
         request,
         'blog/category.html',
